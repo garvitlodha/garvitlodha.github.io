1538675aafd415b9e29a44d02858fdf4f342cb52
Garvit Lodha <sgarvit87@gmail.com>
2026-09-13 13:30:24 +0530

    Delete script.js

--- script.js
+++
@@ -1,1 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{const p=window.location.pathname.split("/").pop()||"index.html";document.querySelectorAll("nav a").forEach(a=>{if(a.getAttribute("href")===p)a.classList.add("active")})});
